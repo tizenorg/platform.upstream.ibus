2b3b198b1b6d313710bb424d8c8d8f273d2d66fa
Eduardo Lima (Etrunko) <eduardo.lima@intel.com>
2013-10-31 14:40:48 -0200

    Remove X11-related warning message
    Change-Id: Ida1209ba2fe86a3fd62400906390a6900585267f
    Signed-off-by: Eduardo Lima (Etrunko) <eduardo.lima@intel.com>

--- src/ibusshare.c
+++ src/ibusshare.c
@@ -111,10 +111,7 @@ ibus_get_socket_path (void)
             display = g_strdup (_display);
         }
 
-        if (display == NULL) {
-            g_warning ("DISPLAY is empty! We use default DISPLAY (:0.0)");
-        }
-        else {
+        if (display != NULL) {
             p = display;
             hostname = display;
             for (; *p != ':' && *p != '\0'; p++);
